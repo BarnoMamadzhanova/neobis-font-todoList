ff9ec31acff54c3b6f131429db977f6b6a10f6d7
BarnoMamadzhanova <b@mamadzhanova.com>
2024-05-12 02:05:03 +0200

    implemented functionalitz of deleting a task

--- script/main.js
+++ script/main.js
@@ -1,10 +1,13 @@
 let todoList = [];
 
+const tasks = document.getElementById("tasks");
+
 class Task {
-  constructor(id, name, category) {
+  constructor(id, name, category, status = false) {
     this.id = id;
     this.name = name;
     this.category = category;
+    this.status = status;
   }
 }
 
@@ -25,11 +28,10 @@ addNewTask.addEventListener("submit", (e) => {
     alert("Please give a name or select a category!");
   } else {
     alert("Saved!");
-    todoList.push(
-      new Task(generateRandomId(), name.value, selectedCategory.value)
-    );
 
-    displayNewTask();
+    let task = new Task(generateRandomId(), name.value, selectedCategory.value);
+    todoList.push(task);
+    displayNewTask(task);
 
     name.value = "";
     selectedCategory.checked = false;
@@ -45,7 +47,19 @@ function generateRandomId() {
   return randomID;
 }
 
-function displayNewTask() {}
+function displayNewTask(task) {
+  let newTask = `<div class="task task-single" data-id="${task.id}">
+          <span class="task__category">${task.category}</span>
+          <p class="task__name">${task.name}</p>
+          <button class="task__button task__button--edit" onclick="editTask('${task.id}')">Edit</button>
+          <button class="task__button task__button--delete" onclick="deleteTask('${task.id}')">Delete</button>
+        </div>`;
+  tasks.insertAdjacentHTML("beforeend", newTask);
+}
 
-// Example usage
-// console.log(generateRandomId());
+function deleteTask(id) {
+  let isNotThisId = (elem) => elem.id !== id;
+  todoList = todoList.filter(isNotThisId);
+
+  document.querySelector(`.task-single[data-id="${id}"]`).remove();
+}
